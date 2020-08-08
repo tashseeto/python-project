d6b9ef425b032d759a0a38ad38166e536e10f4d3
Natasha Seeto <tashseeto@gmail.com>
2020-08-08 22:38:41 +1000

    working on function - process_weather

--- python-project-starter/part1/part1-working.py
+++ python-project-starter/part1/part1-working.py
@@ -9,9 +9,48 @@ for category, categorydata in forecast.items():
     if category == "DailyForecasts":
         day_data.append(categorydata)
 # print(day_data)
-  
-daily_info = []
-for days in day_data:
+
+
+ 
+date_ISO = []
+count = 0
+for days in day_data[0:4]:
+    print(days)
+   
+
+
+
+
+
+
+# count = 0
+# for days, values in day_data[0][0]:
+#     if days == "Date":
+#         date_ISO.append(values)
+#         count += 1
+    # this prints all dates
+    
+# print(date_ISO)
+    
+    # for key in days[0]:
+    #     if key == "Date":
+    #         date_ISO.append(days[0][key])
+
+# #         elif key in days[1]:
+#             if key == "Date":
+#                 date_ISO.append(days[1][key])
+
+#         # print(key, days[0][key])
+# print(date_ISO)
+            # above prints keys and values for fri 19 yippee!
+
+# , days[1], days[2], days[3], days[4]
+
+
+
+
+
+
     # print(days[0])
     #  days[1], days[2], days[3], days[4])
     # this prints all dictionaries in list
